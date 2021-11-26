7535c6c81466cfbcca18763de6952a4bd49f0e99
Arden007 <91262831+Arden007@users.noreply.github.com>
2021-11-26 15:15:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// giving the function
+/*
+let abt = document.querySelector(".About");
+abt.style.backgroundColor = "red";
+
+// let abtHead = document.querySelectorAll(".About-head");
+// abtHead.style.color = "black";
+
+let resume = document.querySelector(".Resume");
+resume.style.backgroundColor = "red";
+
+let project = document.querySelector(".Projects");
+project.style.backgroundColor = "red";
+
+let contact = document.querySelector(".Contact");
+contact.style.backgroundColor = "red";
+
+let testimonial = document.querySelector(".Testimonial");
+testimonial.style.backgroundColor = "red";
+
+// declaring when the function should take place
